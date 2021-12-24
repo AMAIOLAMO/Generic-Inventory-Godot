f52d86347a807f3f20774c19ae8bd34093863a2e
CXRedix <1135058679@qq.com>
2021-12-24 11:02:59 +0800

    Clean up code

--- Src/ItemInfo.gd
+++ Src/ItemInfo.gd
@@ -2,9 +2,6 @@ extends Resource
 
 class_name ItemInfo
 
-func can_stack_together(infoA, infoB) -> bool:
-	return infoA.name == infoB.name
-
 export var name: String
 export var texture: Texture
 export var stackLimit: int
