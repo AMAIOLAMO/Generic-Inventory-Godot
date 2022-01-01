5f46f14ce78ee071de7ae356e3e962468bada487
CXRedix <1135058679@qq.com>
2022-01-01 22:18:49 +0800

    Added Item groups

--- Src/ItemInfo.gd
+++ Src/ItemInfo.gd
@@ -2,6 +2,15 @@ extends Resource
 
 class_name ItemInfo
 
+func is_in_group(group: String) -> bool:
+	if _is_in_built_in_group(group): return true
+	return itemGroup.find(group) != -1
+
+func _is_in_built_in_group(group: String) -> bool:
+	return false
+
 export var name: String
 export var texture: Texture
 export var stackLimit: int
+
+export(Array, String) var itemGroup: Array
